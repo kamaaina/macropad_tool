a36fee3f1bdf5f1242950ecf257a554546486858
mike <mike808@gmail.com>
2024-04-16 19:36:09 +0900

    fix validate for mapping

--- src/mapping.rs
+++ src/mapping.rs
@@ -102,9 +102,15 @@ impl Mapping {
                 }
 
                 // check the individual button
-                for btn in btn_mapping {
-                    println!("btn: {}", btn);
-                    Self::validate_key_mapping(btn.to_string())?;
+                for (k, btn) in btn_mapping.iter().enumerate() {
+                    if Self::validate_key_mapping(btn.to_string()).is_err() {
+                        return Err(anyhow!(
+                            "error at layer {} row {} button {}",
+                            i + 1,
+                            j + 1,
+                            k + 1
+                        ));
+                    }
                 }
             }
 
@@ -124,7 +130,7 @@ impl Mapping {
 
     fn validate_key_mapping(key: String) -> Result<()> {
         // ensure we don't go over max
-        let keys: Vec<_> = key.split('-').collect();
+        let keys: Vec<_> = key.split(',').collect();
         if keys.len() > consts::MAX_KEY_PRESSES {
             return Err(anyhow!(
                 "One key can be mapped to a maximum of {} key presses",
@@ -134,28 +140,33 @@ impl Mapping {
 
         // check individual keys
         for k in keys {
-            let da_key = Self::uppercase_first(k);
-            println!("da_key: {da_key}");
-            // could be media, control, or regular key
-            let mut found = false;
-            for i in 0..3 {
-                match i {
-                    0 => {
-                        found = Self::is_control_key(&da_key);
+            let single_key: Vec<_> = k.split('-').collect();
+            for sk in single_key {
+                let da_key = Self::uppercase_first(sk);
+                // could be media, control, or regular key
+                let mut found = false;
+                for i in 0..3 {
+                    match i {
+                        0 => {
+                            found = Self::is_modifier_key(&da_key);
+                        }
+                        1 => {
+                            found = Self::is_media_key(&da_key);
+                        }
+                        2 => {
+                            found = Self::is_regular_key(&da_key);
+                        }
+                        _ => {
+                            panic!("unaccounted key test")
+                        }
                     }
-                    1 => {
-                        found = Self::is_media_key(&da_key);
-                    }
-                    2 => {
-                        found = Self::is_regular_key(&da_key);
-                    }
-                    _ => {
-                        panic!("unaccounted key test")
+                    if found {
+                        break;
                     }
                 }
-            }
-            if !found {
-                return Err(anyhow!("unknown key - {}", k));
+                if !found {
+                    return Err(anyhow!("unknown key - {}", sk));
+                }
             }
         }
         Ok(())
@@ -175,8 +186,7 @@ impl Mapping {
         result
     }
 
-    fn is_control_key(keystr: &String) -> bool {
-        println!("******** {keystr}");
+    fn is_modifier_key(keystr: &String) -> bool {
         let ck = Modifier::from_str(&keystr);
         if ck.is_ok() {
             return true;
